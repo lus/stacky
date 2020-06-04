a8850e19efc818448c737f2f5b81969e3064410e
Lukas SP <kbrt@protonmail.com>
2020-06-04 20:13:37 +0200

    Add further logging

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ import (
 
 func main() {
 	// Load the current application configuration
+	log.Println("Loading the application configuration...")
 	err := config.Load()
 	if err != nil {
 		log.Panicln(err)
 	}
 
 	// Connect to the configured MongoDB host
+	log.Println("Connecting to the configured MongoDB host...")
 	err = database.Connect(config.CurrentConfig.MongoDBURI)
 	if err != nil {
 		log.Panicln(err)
