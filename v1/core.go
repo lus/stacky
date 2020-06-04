a7ba2b74d3112a2de9010e89b1655515b51969f4
Lukas SP <kbrt@protonmail.com>
2020-06-04 22:29:03 +0200

    Implement stack host listing endpoint

--- v1/core.go
+++ v1/core.go
@@ -6,4 +6,5 @@ import routing "github.com/fasthttp/router"
 func Initialize(router *routing.Router) {
 	// Initialize the API endpoints
 	router.GET("/stacks/{name}", authenticated(endGetStack, 1))
+	router.GET("/stacks/{name}/hosts", authenticated(endGetStackHosts, 1))
 }
